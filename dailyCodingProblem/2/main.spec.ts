2fa6fa2a1dbe44c7ef0367d3c74988c00037fcb6
Andrii Koliaka <andrei.koliaka@gettyimages.com>
2022-12-14 21:16:50 +0100

    Daily Coding Problem - Hard - Problem 2 - fix typo

--- dailyCodingProblem/2/main.spec.ts
+++ dailyCodingProblem/2/main.spec.ts
@@ -1,7 +1,7 @@
 import getIntegerProduct from './main';
 
 describe('2', () => {
-    it('should return return correct array', () => {
+    it('should return correct array', () => {
         expect(getIntegerProduct([1, 2, 3, 4, 5])).toEqual([120, 60, 40, 30, 24]);
         expect(getIntegerProduct([3, 2, 1])).toEqual([2, 3, 6]);
     });
